187fa0f8dce74a3fc8c79f86ad594e870604cc59
tubarklol <thomas.barkley@hotmail.co.uk>
2024-06-14 23:46:37 +0100

    Main-Tidied-Up

--- Program1.cs
+++ Program1.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text; 
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace Tetris 
 {
@@ -13,48 +14,14 @@ namespace Tetris
     {
         static void Main(string[] args) //method/functions
         {
-            bool on = true; 
-            GameGrid grid = new GameGrid(20,10); 
-            for(int i = 0; i<grid.Rows; i++)
-            {
-                if(i%2==0)
-                {
-                    for(int j = 0; j<grid.Columns; j++)
-                    {
-                        grid[i,j] = 1; 
-                    }
-                }
-                else
-                {
-                    for(int j = 0; j<grid.Columns; j++)
-                    {
-                        if(on==true)
-                        {
-                            grid[i,j] = 1; 
-                            on = false; 
-                        }
-                        else
-                        {
-                            grid[i,j] = 0; 
-                            on = true; 
-                        }
-                    }
-                }
-            }
+            GameGrid grid = new GameGrid(10,10); 
+            grid.populateGridWithBasicData(); 
             grid.printGridContents(); 
-            grid.clearRowMoveDown(19); 
             Console.WriteLine(); 
             Console.WriteLine("///////////////////////////////////");
             Console.WriteLine(); 
+            grid.clearAllFullRows(); 
             grid.printGridContents(); 
-
-            // Console.WriteLine(grid.isEntireRowFull(0)); 
-            // Console.WriteLine(grid.isEntireRowFull(1));
-            // Console.WriteLine(grid.isEntireRowEmpty(0));  
-            // Console.WriteLine(grid[0,0]);
-            // Console.WriteLine(grid.isEmpty(0,0));
-            // Console.WriteLine(grid[0,1]); 
-            // Console.WriteLine(grid.isEmpty(0,1)); 
         }
     }
 }
