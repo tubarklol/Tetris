7f37405c345e082f72ba0eaffa1d5d03355ea629
tubarklol <thomas.barkley@hotmail.co.uk>
2024-06-15 16:15:21 +0100

    Rotation-Move-And-Reset-Functions-Added

--- Block.cs
+++ Block.cs
@@ -1,7 +1,6 @@
 using System.Runtime;
-using Tetris;
 
-namespace Block 
+namespace Tetris 
 {
     public abstract class Block 
     {
@@ -11,6 +10,50 @@ namespace Block
         public abstract int Id {get; }
 
         private int currentRotationState;
-        private int currentOffset;
+        private Position currentOffset;
+
+        public Block()
+        {
+            currentOffset = new Position(StartOffset.Row, StartOffset.Column); 
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach(Position p in Tiles[currentRotationState])
+            {
+                yield return new Position(p.Row + StartOffset.Row, p.Column + StartOffset.Column); 
+            }
+        }
+
+        public void rotate90DegreesClockwise()
+        {
+            currentRotationState = (currentRotationState + 1) % Tiles.Length; 
+        }
+
+        public void rotate90DegreesAntiClockwise()
+        {
+            if(currentRotationState==0)
+            {
+                currentRotationState = Tiles.Length - 1; 
+            }
+            else
+            {
+                currentRotationState--; 
+            }
+
+        }
+
+        public void Move(int r, int c)
+        {
+            currentOffset.Row += r; 
+            currentOffset.Column += c; 
+        }
+
+        public void reset()
+        {
+            currentOffset.Row = StartOffset.Row; 
+            currentOffset.Column = StartOffset.Column; 
+            currentRotationState = 0; 
+        }
     }
 }
